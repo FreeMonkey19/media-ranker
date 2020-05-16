516bcd69f885e0ff4814d4a613b482c6a21f18fc
freeMonkey19 <freeMonkey19@gmail.com>
2020-05-15 23:01:22 -0700

    add resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,33 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/create'
-  get 'works/index'
-  get 'works/show'
-  get 'works/edit'
-  get 'works/create'
+  resources :works
+  resources :users
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+# get 'users/index'
+# get 'users/show'
+# get 'users/edit'
+# get 'users/create'
+# get 'works/index'
+# get 'works/show'
+# get 'works/edit'
+# get 'works/create'
+
+
+
+
+
+
+# Rails.application.routes.draw do
+#   get    "/books"          , to: "books#index",   as: :book
+#   post   "/books"          , to: "books#create"
+#   get    "/books/new"      , to: "books#new",     as: :new_book
+#   get    "/books/:id"      , to: "books#show",    as: :book
+#   patch  "/books/:id"      , to: "books#update"
+#   put    "/books/:id"      , to: "books#update"
+#   delete "/books/:id"      , to: "books#destroy"
+#   get    "/books/:id/edit" , to: "books#edit",    as: :edit_book
+# end
