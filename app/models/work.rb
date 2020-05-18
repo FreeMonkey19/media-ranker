939218be82035e18cd264351f1633f3c1dc8d02c
luckyMonkey <freeMonkey19@gmail.com>
2020-05-18 13:39:43 -0700

    create top-ten-method - create main page columns - style main page

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,4 +6,14 @@ class Work < ApplicationRecord
   # https://apidock.com/rails/ActiveRecord/Validations/ClassMethods/validates_uniqueness_of
   validates_uniqueness_of :title, scope: [:category]
 
+  # I did not do this on my own, but instead had help from a mentor.
+  # I know the directions said to use .sample then change later - 
+  # but mentors sometimes do what they want! 
+  def self.top_ten(type)
+    category_list = Work.where(category: type)
+    sorted = category_list.sort_by {|x| x.votes.length}.reverse
+    ten = sorted.first(10)
+    return ten
+  end
+
 end
