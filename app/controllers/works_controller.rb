47d1162c5ae68005770fbb2e57b66ee5878bd20e
luckyMonkey <freeMonkey19@gmail.com>
2020-05-17 18:25:04 -0700

    create new work - save to db - implement form validations

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,41 +1,72 @@
 class WorksController < ApplicationController
-
   def main
     @works = Work.order(:category)
-	end
-	
+  end
+
   def index
     @works = Work.all
     if params[:search]
-    	@works = Work.search(params[:search]).order("created_at DESC")
-    else 
+      @works = Work.search(params[:search]).order("created_at DESC")
+    else
       @works = Work.all.order("created_at DESC")
     end
   end
 
-	def show
-		id = params[:id]
-    @work = Work.find_by(id: id)
+  def show
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
 
     if @work.nil?
       redirect_to works_path
       return
     end
+  end
 
-	end
- 
- 
-
-  def edit
+  def new # renders form for create action
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(
+      work_params
+    )
+    if @work.save
+      redirect_to works_path
+      return
+    else
+      render :new
+      return
+    end
+	end
+
+  def edit
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
   end
 
+  def update
+    result = @work.update(work_params)
+    if result
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # work = Work.find_by(id; params[;id]).destroy
+    # redirect_to works_path
+    # return
+  end
 
   private
+
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
-
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+  validates :category, :creator, :publication_year, :description, presence: true
+  validates :title, presence: true, uniqueness: true
 end
