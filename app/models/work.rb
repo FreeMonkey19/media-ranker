602932005ace6ac27782378bda269666c7aa0e3f
luckyMonkey <freeMonkey19@gmail.com>
2020-05-18 21:38:23 -0700

    test all five validations for presence and one for uniqueness

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,13 +2,10 @@ class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
   validates :category, :creator, :title, :publication_year, :description, presence: true
-  validates :publication_year, numericality: { only_integer: true }
   # https://apidock.com/rails/ActiveRecord/Validations/ClassMethods/validates_uniqueness_of
+  # only one of same title in each category
   validates_uniqueness_of :title, scope: [:category]
 
-  # I did not do this on my own, but instead had help from a mentor.
-  # I know the directions said to use .sample then change later - 
-  # but mentors sometimes do what they want! 
   def self.top_ten(type)
     category_list = Work.where(category: type)
     sorted = category_list.sort_by {|x| x.votes.length}.reverse
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -12,16 +12,53 @@ describe Work do
       expect(@work.valid?).must_equal true
     end
 
-    it "fails validation when there is a missing field" do
+    it "fails validation when there is a missing title" do
       @work.title = nil
 
       expect(@work.valid?).must_equal false
+      expect(@work.errors.messages.include?(:title)).must_equal true
+      expect(@work.errors.messages[:title].include?("can't be blank")).must_equal true
     end
 
-    it "fails validation when the title already exists in the same category" do
+    it "fails validation when a title already exists in the same category" do
       Work.create(category: @work.category, creator: @work.creator, title: @work.title, publication_year: @work.publication_year, description: @work.description)
 
       expect(@work.valid?).must_equal false
+      expect(@work.errors.messages.include?(:title)).must_equal true
+      expect(@work.errors.messages[:title].include?("has already been taken")).must_equal true
    end
+   
+   it "fails validation when there is a missing category" do
+      @work.category = nil
+
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages.include?(:category)).must_equal true
+      expect(@work.errors.messages[:category].include?("can't be blank")).must_equal true
+   end
+
+   it "fails validation when there is a missing creator" do
+      @work.creator = nil
+
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages.include?(:creator)).must_equal true
+      expect(@work.errors.messages[:creator].include?("can't be blank")).must_equal true
+   end
+
+   it "fails validation when there is a missing description" do
+      @work.description = nil
+
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages.include?(:description)).must_equal true
+      expect(@work.errors.messages[:description].include?("can't be blank")).must_equal true
+   end
+
+   it "fails validation when there is a missing publication_year" do
+      @work.publication_year = nil
+
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages.include?(:publication_year)).must_equal true
+      expect(@work.errors.messages[:publication_year].include?("can't be blank")).must_equal true
+   end
+
   end
 end
