763e211760669b831a81c618f77a4d8229db4ba1
luckyMonkey <freeMonkey19@gmail.com>
2020-05-18 20:31:08 -0700

    model test - fails when title already exists in same catefory- passing

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -17,5 +17,11 @@ describe Work do
 
       expect(@work.valid?).must_equal false
     end
+
+    it "fails validation when the title already exists in the same category" do
+      Work.create(category: @work.category, creator: @work.creator, title: @work.title, publication_year: @work.publication_year, description: @work.description)
+
+      expect(@work.valid?).must_equal false
+   end
   end
 end
