3199a8ae7ad070c06cb49ddbef4710465532ea34
luckyMonkey <freeMonkey19@gmail.com>
2020-05-20 13:44:04 -0700

    add spotlight winner method

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,10 +1,17 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :users, through: :votes
   validates :category, :creator, :title, :publication_year, :description, presence: true
   # https://apidock.com/rails/ActiveRecord/Validations/ClassMethods/validates_uniqueness_of
   # only one of same title in each category
   validates_uniqueness_of :title, scope: [:category]
+
+
+  def self.winner
+    @works = Work.order(:created_at)
+    @works.max_by {|work| work.votes.length}
+  end
+
 
   def self.top_ten(type)
     category_list = Work.where(category: type)
@@ -12,5 +19,7 @@ class Work < ApplicationRecord
     ten = sorted.first(10)
     return ten
   end
+
+
 
 end
