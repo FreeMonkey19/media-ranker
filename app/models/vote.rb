87b820bd90604e8b54aa4dd77215820984889800
luckyMonkey <freeMonkey19@gmail.com>
2020-05-20 11:41:43 -0700

    add validation to votes model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
+# user can vote one time on each work - prevent duplicate voting
+  validates_uniqueness_of :work_id, scope: [:user_id]
 end
