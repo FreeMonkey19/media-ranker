5e98b5ef499d2eb60c06501bd6699a0e3b31abe9
luckyMonkey <freeMonkey19@gmail.com>
2020-05-19 22:35:47 -0700

    create login/logout actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,46 @@
 class UsersController < ApplicationController
-  def index
+  def login_form
+    @user = User.new
   end
 
-  def show
+  def login
+    # try to find the user
+    user = User.find_by(name: params[:user][:name])
+
+    if user.nil?
+      # New User
+      user = User.new(name: params[:user][:name])
+      # if not saved 
+      if ! user.save
+        flash[:error] = "Unable to login"
+        redirect_to root_path
+        return
+      end
+      # if saved
+      flash[:welcome] = "Welcome #{user.name}"
+    else
+      # Existing User
+      flash[:welcome] = "Welcome back #{user.name}"
+    end
+
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
-  def edit
+  def logout
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:notice] = "Goodbye #{user.name}"
+      else
+        session[:user_id] = nil
+        flash[:notice] = "Error: Unknown User"
+      end 
+    else
+      flash[:error] = "You must be logged in - to logout"
+    end
+    redirect_to root_path
   end
 
-  def create
-  end
 end
