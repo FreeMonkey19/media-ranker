283909225a218278d053df08dd50b35fd44184aa
luckyMonkey <freeMonkey19@gmail.com>
2020-05-19 22:38:32 -0700

    test users controller with sessions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,24 +1,50 @@
 require "test_helper"
 
 describe UsersController do
-  it "must get index" do
-    get users_index_url
+
+  it "can get the login form" do
+    get login_path
+
     must_respond_with :success
   end
 
-  it "must get show" do
-    get users_show_url
-    must_respond_with :success
+  describe "logging in" do
+    it "can login a new user" do
+      # declare user outside of the expect block
+      # to be used in the entire it block
+      user = nil
+      expect {
+        user = login()
+    }.must_differ "User.count", 1
+
+    must_respond_with :redirect
+    
+    expect(user).wont_be_nil
+    expect(session[:user_id]).must_equal user.id
+    expect(user.name).must_equal "Grace Hopper"
+    end
+
+    it "can login an existing user" do
+      user = User.create(name: "Ed Sheeran")
+
+      expect {
+        login(user.name)
+    }.wont_change "User.count"
+
+    expect(session[:user_id]).must_equal user.id
+    end
   end
 
-  it "must get edit" do
-    get users_edit_url
-    must_respond_with :success
-  end
+    describe "logout" do 
+      it "can logout a logged in user" do
+        #Arrange
+        login()
+        expect(session[:user_id]).wont_be_nil
 
-  it "must get create" do
-    get users_create_url
-    must_respond_with :success
-  end
+        #Act
+        post logout_path
 
-end
+        expect(session[:user_id]).must_be_nil
+      end
+    end
+  end
