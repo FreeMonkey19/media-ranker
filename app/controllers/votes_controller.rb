440e6d5b758c7c010ce372d6fca6d1b5c4dbc66e
luckyMonkey <freeMonkey19@gmail.com>
2020-05-21 16:42:52 -0700

    add votes controller and various files came with the generate command

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,25 @@
+class VotesController < ApplicationController
+
+  def index 
+    @votes = Vote.order(:id)
+end
+
+  def create
+    if session[:user_id] == nil
+      flash[:error] = "Must be logged in to vote"
+      redirect_to root_path
+    else
+      @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
+      if @vote.save
+        flash[:success] = "Success: Upvoted!"
+        redirect_to works_path(params[:work_id])
+      else
+        flash[:error] = "Error: Not Upvoted!"
+        redirect_to works_path(params[:work_id])
+      end
+    end
+  end
+
+end
+
+
