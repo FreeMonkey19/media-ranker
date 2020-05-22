57f8df3f07978b478cf167336135089c24dc3f74
luckyMonkey <freeMonkey19@gmail.com>
2020-05-21 22:06:03 -0700

    save work in order to deploy

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,21 +4,21 @@ class VotesController < ApplicationController
     @votes = Vote.order(:id)
 end
 
-  def create
-    if session[:user_id] == nil
-      flash[:error] = "Must be logged in to vote"
-      redirect_to root_path
+def create
+  if session[:user_id] == nil
+    flash[:error] = "Must be logged in to vote"
+    redirect_to root_path
+  else
+    @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_to works_path(params[:work_id])
     else
-      @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
-      if @vote.save
-        flash[:success] = "Success: Upvoted!"
-        redirect_to works_path(params[:work_id])
-      else
-        flash[:error] = "Error: Not Upvoted!"
-        redirect_to works_path(params[:work_id])
-      end
+      flash[:error] = "Vote not recorded"
+      redirect_to works_path(params[:work_id])
     end
   end
+end
 
 end
 
