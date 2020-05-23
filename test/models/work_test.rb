55d8a284183e962adefd09bf0b545f397d5ea6b7
luckyMonkey <freeMonkey19@gmail.com>
2020-05-22 17:45:05 -0700

    add relations test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -5,9 +5,9 @@ describe Work do
  
 
     before do 
-      @album1 = works(:magic)
-      @album2 = works(:teenage)
-      @album3 = works(:pump)
+      @album1 = works(:album1)
+      @album2 = works(:album2)
+      @album3 = works(:album3)
       @user1 = users(:user1)
       @user2 = users(:user2)
       @user3 = users(:user3)
@@ -41,18 +41,8 @@ describe Work do
       expect(new_album.errors.messages.include?(:title)).must_equal true
       expect(new_album.errors.messages[:title].include?("can't be blank")).must_equal true
     end
-  
-
-    it "fails validation when a title already exists in the same category" do
-      new_album.title = @album1.title
-
-      expect(new_album.valid?).must_equal false
-      expect(new_album.errors.messages.include?(:title)).must_equal true
-      expect(new_album.errors.messages[:title].include?("has already been taken")).must_equal true
-   end
-
-   
-   it "fails validation when there is a missing category" do
+    
+    it "fails validation when there is a missing category" do
       new_album.category = nil
 
       expect(new_album.valid?).must_equal false
@@ -87,12 +77,30 @@ describe Work do
       expect(new_album.errors.messages[:publication_year].include?("can't be blank")).must_equal true
    end
 
+   it "fails validation when a title already exists in the same category" do
+    new_album.title = @album1.title
+
+    expect(new_album.valid?).must_equal false
+    expect(new_album.errors.messages.include?(:title)).must_equal true
+    expect(new_album.errors.messages[:title].include?("has already been taken")).must_equal true
+   end
+
    it "passes validation with same title but two different categories" do
       Work.create!(category: "book", title: new_album.title, creator: new_album.creator, publication_year: 2020, description: "new book same title as album")
 
       expect(new_album.valid?).must_equal true
       expect(new_album.errors.messages).must_be_empty
+   end
+  end
+
+  describe "relations" do
+    it "can have many votes" do
+      expect(@album2.votes.count).must_equal 3
+      @album2.votes.each do |vote|
+        expect(vote).must_be_instance_of Vote
+      end
     end
   end
-  
 end
+  
+
