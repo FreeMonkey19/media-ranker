29d919f2152bae0885df7739b4f537a4a92f2f31
luckyMonkey <freeMonkey19@gmail.com>
2020-05-26 10:29:03 -0700

    remove comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,25 +17,19 @@ class UsersController < ApplicationController
   end
 
   def login
-    # try to find the user
     user = User.find_by(name: params[:user][:name])
 
     if user.nil?
-      # New User
       user = User.new(name: params[:user][:name])
-      # if not saved 
       if ! user.save
         flash[:error] = "Unable to login"
         redirect_to root_path
         return
       end
-      # if saved
       flash[:welcome] = "Welcome #{user.name}"
     else
-      # Existing User
       flash[:welcome] = "Welcome back #{user.name}"
     end
-
     session[:user_id] = user.id
     redirect_to root_path
   end
